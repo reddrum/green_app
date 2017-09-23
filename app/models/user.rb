ba664f523206f0991943d7397ba330f1fdf7ae45
“reddrum” <anton.kranz@me.com>
2017-09-23 17:23:44 +0300

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,6 +11,17 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
+
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
   # Возвращает дайджест для указанной строки.
   class << self
@@ -32,13 +45,26 @@ class User < ApplicationRecord
   end
 
   # Возвращает true, если указанный токен соответствует дайджесту.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Забывает пользователя
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    # Преобразует адрес электронной почты в нижний регистр.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Создает и присваивает токен активации и его дайджест.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
